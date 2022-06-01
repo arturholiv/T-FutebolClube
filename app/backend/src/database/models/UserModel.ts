cba9913ef38fc61634ed3f3cc372d1af9265b3a9
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-06-01 15:34:32 -0300

    feat: implements users migrations, models, and seeders - req 3

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,23 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  public username: string;
+  public email: string;
+  public role: string;
+  public password: string;
+}
+
+Users.init({
+  username: DataTypes.STRING,
+  role: DataTypes.STRING,
+  email: DataTypes.STRING,
+  password: DataTypes.STRING,
+}, {
+  timestamps: false,
+  underscored: true,
+  sequelize: db,
+  modelName: 'Users',
+});
+
+export default Users;
